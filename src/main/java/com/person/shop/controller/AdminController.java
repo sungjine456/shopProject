f7e39cf1aeeb0ad48090b7fcf4bd0f27c9289454
sungjine456 <sungjine456@gmail.com>
2017-05-02 20:41:34 +0900

    security 적용중

---
+++ src/main/java/com/person/shop/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.person.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	@RequestMapping("/")
+	public String adminTest(){
+		return "index.html";
+	}
+}
---
+++ src/main/java/com/person/shop/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.person.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+	@RequestMapping("/")
+	public String test(){
+		return "index.html";
+	}
+}
