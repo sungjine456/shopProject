a86eda21ad76bb6b2d4dea0ca2e57a7e4b7e0950
sungjine456 <sungjine456@gmail.com>
2017-05-03 21:28:35 +0900

    h2 database, jpa 연동 & 테스트

---
+++ src/main/java/com/person/shop/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.person.shop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.person.shop.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+	User findUserByIdx(long idx);
+}
---
+++ src/test/java/com/person/shop/repository/UserRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.person.shop.repository;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.person.shop.ShopProjectApplication;
+import com.person.shop.domain.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ShopProjectApplication.class)
+@Sql({"classpath:init.sql"})
+public class UserRepositoryTest {
+
+	@Autowired private UserRepository userRepository;
+	
+	@Test
+	public void findUserByIdTest() {
+		User user = userRepository.findUserByIdx(1L);
+		
+		assertThat(user.getEmail(), is("email"));
+		assertThat(user.getName(), is("name"));
+		assertThat(user.getPassword(), is("admin"));
+	}
+}
