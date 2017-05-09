b7802bbc1ccddc4f89a5528bdebbfa52a6e856f1
sungjine456 <sungjine456@gmail.com>
2017-05-09 18:41:30 +0900

    Revert "ftl 파일을 webapp폴더에서 resources폴더로 이동"
    This reverts commit d02fa8ce42dc50adc29db90009bd63d372caea32.

--- src/main/java/com/person/shop/conf/AppConfig.java
+++ src/main/java/com/person/shop/conf/AppConfig.java
@@ -1,5 +1,8 @@
 package com.person.shop.conf;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -7,11 +10,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import freemarker.template.utility.XmlEscape;
 
 @Configuration
 @EnableWebMvc
 public class AppConfig extends WebMvcConfigurerAdapter {
+	@Bean(name="freemarkerConfig")
+	public FreeMarkerConfigurer freemarkerConfig() {
+		FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
+		configurer.setTemplateLoaderPath("/");
+		Map<String, Object> map = new HashMap<>();
+		map.put("xml_escape", new XmlEscape());
+		configurer.setFreemarkerVariables(map);
+		return configurer;
+	}
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.freeMarker();
+	}
+	
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
